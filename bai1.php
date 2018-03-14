4e4fff13806d6703f36dff4d193e21740d0ac50e
aseanzdragon <nguyenluongquang1997@gmail.com>
2018-03-14 16:43:26 +0700

    Bai1
    abc

---
+++ bai1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        
+       $str = ' Chào mừng các bạn đến với môn học lập trình web 1  ' ;
+       echo $str."<br>";
+   
+       echo nl2br ( "Chào\n mừng\n các\n bạn\n đến\n với\n môn\n học\n lập\n trình\n web\n 1 ");
+        ?>
+    </body>
+</html>
+
+
